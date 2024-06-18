9c10bee5334e5cae95869e765b2ae60ce32f46b7
khmerweb <vuthdevelop1@gmail.com>
2024-06-19 02:06:00 +0700

    Make it better

---
+++ .vitepress/theme/Layout.vue
@@ -0,0 +1,18 @@
+<script setup>
+import { useData } from 'vitepress'
+
+// https://vitepress.dev/reference/runtime-api#usedata
+const { site, frontmatter } = useData()
+</script>
+
+<template>
+  <link href="/fonts/setup.css" rel="stylesheet" />
+  <div v-if="frontmatter.home">
+    <h1>{{ site.title }}</h1>
+    <p>{{ site.description }}</p>
+  </div>
+  <div v-else>
+    <a href="/">Home</a>
+    <Content />
+  </div>
+</template>
